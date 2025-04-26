d1e3c13f95bb0c55575aa13aaa1fdd5e06e7e3b3
Oleksandr Mezhenskyi <alexmezhenskyi@gmail.com>
2025-04-27 00:49:12 +0200

    feat: update cors proxy vercel

--- api/proxy.ts
+++ api/proxy.ts
@@ -8,7 +8,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return
   }
 
-  // Handling preflight requests
+  // Handling preflight (CORS OPTIONS) requests
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
@@ -28,17 +28,20 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const buffer = await response.arrayBuffer()
 
     res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
     res.setHeader('Content-Type', contentType)
 
-    // Selecting a caching strategy based on content type
+    // Select a caching strategy based on content type
     if (contentType.includes('image') || contentType.includes('video')) {
       res.setHeader('Cache-Control', 'public, max-age=86400, immutable') // 1 day
     } else if (contentType.includes('application/json')) {
       res.setHeader('Cache-Control', 'public, max-age=300') // 5 minutes
-    } else if (contentType.includes('text/xml') || contentType.includes('application/xml')) {
+    } else if (contentType.includes('text/xml') || contentType.includes('application/xml') || contentType.includes('text/vtt')) {
       res.setHeader('Cache-Control', 'public, max-age=600') // 10 minutes
     } else {
-      res.setHeader('Cache-Control', 'no-cache') // Everything else is without caching
+      res.setHeader('Cache-Control', 'no-cache') // No cache
     }
 
     res.status(200).send(Buffer.from(buffer))
